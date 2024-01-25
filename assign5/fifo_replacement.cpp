7962815b3f4d9942ddd3a2367497510e1de3edb3
KennyLiu5 <157739840+KennyLiu5@users.noreply.github.com>
2024-01-25 11:32:53 -0800

    Completed

--- assign5/fifo_replacement.cpp
+++ assign5/fifo_replacement.cpp
@@ -1,7 +1,7 @@
 /**
 * Assignment 5: Page replacement algorithms
  * @file fifo_replacement.cpp
- * @author ??? (TODO: your name)
+ * @author NAME
  * @brief A class implementing the FIFO page replacement algorithms
  * @version 0.1
  */
@@ -9,26 +9,39 @@
 // Remember to add sufficient and clear comments to your code
 
 #include "fifo_replacement.h"
+#include <iostream>
 
-// TODO: Add your implementation here
 FIFOReplacement::FIFOReplacement(int num_pages, int num_frames)
-: Replacement(num_pages, num_frames)
+: Replacement(num_pages, num_frames) , q()
 {
-    // TODO: Add additional implementation code
 }
 
-// TODO: Add your implementations for desctructor, load_page, replace_page here
 FIFOReplacement::~FIFOReplacement() {
-    // TODO: Add necessary code here
+  while(!q.empty())
+    {
+      q.pop_back();
+    }
 }
 
 // Access an invalid page, but free frames are available
 void FIFOReplacement::load_page(int page_num) {
-    // TODO: Update your data structure FIFO replacement and pagetable
+  page_faults++; // invalid page, so it is a page fault
+  page_table[page_num].valid = true; 
+  q.push_back(page_num); // pushes page_num into vector
+  page_table[page_num].frame_num = frame_count;
+  frame_count++; // adding new page, so increment frame count
 }
 
 // Access an invalid page and no free frames are available
 int FIFOReplacement::replace_page(int page_num) {
-    // TODO: Update your data structure FIFO replacement and pagetable
-    return 0;
+  page_faults++; // invalid page, so it is a page fault
+  int front = q.front(); // gets number in front of vector (first in)
+  page_table[front].valid = false; // change that page entry to false, will be replaced
+  q.erase(q.begin()); // erases element in front - begin is index 0
+
+  page_table[page_num].valid = true; // new page entry
+  q.push_back(page_num);
+  page_replacement++; // page replaced, so increment page replacement
+  
+  return 0;
 }
