449fff9089928aaeef78857ab4c94a84effef73b
KennyLiu5 <157739840+KennyLiu5@users.noreply.github.com>
2024-01-25 11:47:19 -0800

    Completed

--- assign5/pagetable.cpp
+++ assign5/pagetable.cpp
@@ -1,7 +1,7 @@
-/**
+**
 * Assignment 5: Page replacement algorithms
  * @file pagetable.cpp
- * @author ??? (TODO: your name)
+ * @author NAME
  * @brief This class represents a traditional pagetable data structure.
  * @version 0.1
  */
@@ -10,15 +10,19 @@
 
 #include "pagetable.h"
 
-// TODO: Add your implementation of PageTable
 // Constrcutor
-// TODO: Add your code
 PageTable::PageTable(int num_pages) {
-    
+  for(int i=0; i<num_pages; i++)
+    {
+      PageEntry p;
+      pages.push_back(p);
+    }
 }
 
 // Destructor
-// TODO: Add your code
 PageTable::~PageTable() {
-    
+  while(!pages.empty()) // pop all page entries in page table
+    {
+      pages.pop_back();
+    }
 }
