5d0e97cf321a4b5452d44689cbb1b92dde0bdbcc
KennyLiu5 <157739840+KennyLiu5@users.noreply.github.com>
2024-01-25 11:13:57 -0800

    Completed

--- assign3/scheduler_priority.h
+++ assign3/scheduler_priority.h
@@ -1,7 +1,7 @@
 /**
 * Assignment 3: CPU Scheduler
  * @file scheduler_priority.h
- * @author ??? (TODO: your name)
+ * @author NAME
  * @brief This Scheduler class implements the Priority scheduling algorithm.
  * @version 0.1
  */
@@ -13,40 +13,48 @@
 #define ASSIGN3_SCHEDULER_PRIORITY_H
 
 #include "scheduler.h"
+#include <vector>
+#include <queue>
+#include <algorithm>
 
 class SchedulerPriority : public Scheduler {
 private:
-    // TODO: add necessary member variables here for your implementation
+  // TODO: add necessary member variables here for your implementation
+  queue<PCB> Q; // queue of PCBs 
+  vector<PCB> done; // PCBs done in queue , mainly to display 
+  int count; // to count how many processes there are
+  double total_turnaround; // sum of turnaround times for all processes
+  double total_wait; // sum of waiting times for all processes
 
 public:
-    /**
-     * @brief Construct a new SchedulerPriority object
-     */
-    SchedulerPriority();
+  /**
+   * @brief Construct a new SchedulerPriority object
+   */
+  SchedulerPriority();
 
-    /**
-     * @brief Destroy the SchedulerPriority object
-     */
-    ~SchedulerPriority() override;
+  /**
+   * @brief Destroy the SchedulerPriority object
+   */
+  ~SchedulerPriority() override;
 
-    /**
-     * @brief This function is called once before the simulation starts.
-     *        It is used to initialize the scheduler.
-     * @param process_list The list of processes in the simulation.
-     */
-    void init(std::vector<PCB>& process_list) override;
+  /**
+   * @brief This function is called once before the simulation starts.
+   *        It is used to initialize the scheduler.
+   * @param process_list The list of processes in the simulation.
+   */
+  void init(std::vector<PCB>& process_list) override;
 
-    /**
-     * @brief This function is called once after the simulation ends.
-     *        It is used to print out the results of the simulation.
-     */
-    void print_results() override;
+  /**
+   * @brief This function is called once after the simulation ends.
+   *        It is used to print out the results of the simulation.
+   */
+  void print_results() override;
 
-    /**
-     * @brief This function simulates the scheduling of processes in the ready queue.
-     *        It stops when all processes are finished.
-     */
-    void simulate() override;
+  /**
+   * @brief This function simulates the scheduling of processes in the ready queue.
+   *        It stops when all processes are finished.
+   */
+  void simulate() override;
 
 };
 
