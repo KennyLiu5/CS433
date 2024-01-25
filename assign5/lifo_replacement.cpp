49e021df43a285ccfb4231659d9e5af089def6cf
KennyLiu5 <157739840+KennyLiu5@users.noreply.github.com>
2024-01-25 11:38:14 -0800

    Completed

--- assign5/lifo_replacement.cpp
+++ assign5/lifo_replacement.cpp
@@ -1,8 +1,9 @@
+
 /**
 * Assignment 5: Page replacement algorithms
  * @file lifo_replacement.cpp
- * @author ??? (TODO: your name)
- * @brief A class implementing the Last in First Out (LIFO) page replacement algorithms
+ * @author NAME
+ * @brief A class implementing the LIFO page replacement algorithms
  * @version 0.1
  */
 //You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
@@ -10,25 +11,38 @@
 
 #include "lifo_replacement.h"
 
-// TODO: Add your implementation here
 LIFOReplacement::LIFOReplacement(int num_pages, int num_frames)
-: Replacement(num_pages, num_frames)
+: Replacement(num_pages, num_frames), q()
 {
-    // TODO: Add additional implementation code
+  // initialized q line 15
 }
 
-// TODO: Add your implementations for desctructor, load_page, replace_page here
 LIFOReplacement::~LIFOReplacement() {
-    // TODO: Add necessary code here
+  while(!q.empty())
+    {
+      q.pop_back();
+    }
 }
 
 // Access an invalid page, but free frames are available
 void LIFOReplacement::load_page(int page_num) {
-    // TODO: Add necessary code here
+  page_faults++; // invalid page, so it is a page fault
+  page_table[page_num].valid = true; 
+  q.push_back(page_num); // pushes page_num into vector
+  page_table[page_num].frame_num = frame_count;
+  frame_count++; // adding new page, so increment frame count
 }
 
 // Access an invalid page and no free frames are available
 int LIFOReplacement::replace_page(int page_num) {
-    // TODO: Add necessary code here
-    return 0;
+  page_faults++; // invalid page, so it is a page fault
+  int last = q.back(); // gets number at rear of vector (last in)
+  page_table[last].valid = false; // change that page entry to false, will be replaced
+  q.pop_back(); // erases element in rear
+
+  page_table[page_num].valid = true; // new page entry
+  q.push_back(page_num);
+  page_replacement++; // page replaced, so increment page replacement
+  
+  return 0;
 }
