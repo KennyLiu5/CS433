a18a65f1ee139da68bdddc274c85cee2e58c92f7
KennyLiu5 <157739840+KennyLiu5@users.noreply.github.com>
2024-01-25 11:07:00 -0800

    Completed

--- assign3/pcb.h
+++ assign3/pcb.h
@@ -1,7 +1,7 @@
-/**
+**
  * Assignment 1: priority queue of processes
  * @file pcb.h
- * @author ??? (TODO: your name)
+ * @author Jesus Mendoza and Kenny Liu (TODO: your name)
  * @brief This is the header file for the PCB class, a process control block.
  * @version 0.1
  */
@@ -24,42 +24,48 @@ using namespace std;
  */
 class PCB {
 public:
-    // Name of the process
-    string name;
-    // The unique process ID
-    unsigned int id;
-    // The priority of a process. Larger number represents higher priority
-    unsigned int priority;
-    // The CPU burst time of the process
-    unsigned int burst_time;
-    // The arrival time of the process
-    unsigned int arrival_time;
+  // Name of the process
+  string name;
+  // The unique process ID
+  unsigned int id;
+  // The priority of a process. Larger number represents higher priority
+  unsigned int priority;
+  // The CPU burst time of the process
+  unsigned int burst_time;
+  // The arrival time of the process
+  unsigned int arrival_time;
 
-    /**
-     * @brief Construct a new PCB object
-     * @param id: each process has a unique ID
-     * @param priority: the priority of the process in the range 1-50. Larger number represents higher priority
-     * @param state the state of the process.
-     */
-    PCB(string name, unsigned int id = 0, unsigned int priority = 1, unsigned int burst_time = 0) {
-        this->id = id;
-        this->name = name;
-        this->priority = priority;
-        this->burst_time = burst_time;
-        this->arrival_time = 0;
-    }
+  // added: to store wait and turnaround times to display
+  unsigned int turnaround;
+  unsigned int wait;
 
-    /**
-     * @brief Destroy the PCB object.
-     *
-     */
-    ~PCB() {}
+  /**
+   * @brief Construct a new PCB object
+   * @param id: each process has a unique ID
+   * @param priority: the priority of the process in the range 1-50. Larger number represents higher priority
+   * @param state the state of the process.
+   */
+  PCB(string name, unsigned int id = 0, unsigned int priority = 1, unsigned int burst_time = 0) {
+    this->id = id;
+    this->name = name;
+    this->priority = priority;
+    this->burst_time = burst_time;
+    this->arrival_time = 0;
+    this->turnaround = 0;
+    this->wait = 0;
+  }
 
-    /**
-     * @brief Print the PCB object.
-     */
-    void print() {
-        cout << "Process " << id << ": " << name << " has priority " << priority << " and burst time "
-             << burst_time << endl;
-    }
+  /**
+   * @brief Destroy the PCB object.
+   *
+   */
+  ~PCB() {}
+
+  /**
+   * @brief Print the PCB object.
+   */
+  void print() {
+    cout << "Process " << id << ": " << name << " has priority " << priority << " and burst time "
+    << burst_time << endl;
+  }
 };
