93b481ba6d11a0aca8ac860c15a9b15fa47d58a8
KennyLiu5 <157739840+KennyLiu5@users.noreply.github.com>
2024-03-29 23:21:14 -0700

    Update scheduler_rr.h

--- assign3/scheduler_rr.h
+++ assign3/scheduler_rr.h
@@ -2,7 +2,7 @@
 /**
 * Assignment 3: CPU Scheduler
  * @file scheduler_rr.h
- * @author NAME
+ * @author Katie Trinh and Cherishma Jalaparti
  * @brief This Scheduler class implements the RoundRobin (RR) scheduling algorithm.
  * @version 0.1
  */
