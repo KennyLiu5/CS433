ed9af0b56373251190f7bf94dac1cc02d6bc488f
KennyLiu5 <157739840+KennyLiu5@users.noreply.github.com>
2024-03-29 23:20:25 -0700

    DONE

--- assign3/scheduler_priority.cpp
+++ assign3/scheduler_priority.cpp
@@ -1,7 +1,7 @@
 /**
 * Assignment 3: CPU Scheduler
  * @file scheduler_priority.cpp
- * @author NAME
+ * @author Katie Trinh and Cherishma Jalaparti
  * @brief This Scheduler class implements the Priority scheduling algorithm.
  * @version 0.1
  */
